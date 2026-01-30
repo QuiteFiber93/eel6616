33be1ae84c713009d53047012ae125137dd5c11c
QuiteFiber <djd1494@gmail.com>
2026-01-30 01:05:16 -0500

    finished part a and b

--- takehome1/main.m
+++ takehome1/main.m
@@ -1,5 +1,5 @@
 %% Part 1
-clear; clc;
+clear; clc; close all;
 
 % Used for ode45
 tspan = [0, 50];
@@ -13,15 +13,36 @@ omega = @(t) sin(t);
 % disturbance signal
 d = @(t) 0.5*sin(20*t);
 
-% Estimator gain
-g = 1;
-
 % Initial condition
 theta0 = 0;
+
+% Estimator gain
+
+figure
+hold on
+for g = logspace(-1, 0, 3)
 
 % Creating function to be estimated
 estimator = @(t, theta) estimate_dynamics(t, theta, omega(t), d(t), theta_star(t), g);
 
 sol = ode45(estimator, tspan, theta0);
 
-sol.y(end);
+
+plot(sol.x, sol.y, 'DisplayName',['g = ', num2str(g)])
+
+end
+
+for g = logspace(-1, 0, 3)*5
+
+% Creating function to be estimated
+estimator = @(t, theta) estimate_dynamics(t, theta, omega(t), d(t), theta_star(t), g);
+
+sol = ode45(estimator, tspan, theta0);
+
+
+plot(sol.x, sol.y, 'DisplayName',['g = ', num2str(g)])
+
+end
+
+hold off
+legend
