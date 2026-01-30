929a243785eaf57da2026b1948ec74bca4636ffe
QuiteFiber <djd1494@gmail.com>
2026-01-30 01:37:37 -0500

    started part 2

--- takehome1/main.m
+++ takehome1/main.m
@@ -23,7 +23,7 @@ hold on
 for g = [0.1, 1, 5]
 
 % Creating function to be estimated
-estimator = @(t, theta) estimate_dynamics(t, theta, omega(t), d(t), theta_star(t), g);
+estimator = @(t, theta) gradient_estimator(t, theta, omega(t), d(t), theta_star(t), g);
 
 sol = ode45(estimator, tspan, theta0);
 
@@ -49,7 +49,71 @@ hold on
 for g = [0.1, 1, 5]
 
 % Creating function to be estimated
-estimator = @(t, theta) estimate_dynamics(t, theta, omega(t), d(t), theta_star(t), g);
+estimator = @(t, theta) gradient_estimator(t, theta, omega(t), d(t), theta_star(t), g);
+
+sol = ode45(estimator, tspan, theta0);
+
+
+plot(sol.x, sol.y, 'DisplayName',['g = ', num2str(g)])
+
+
+end
+title("w(t) = sin(t) d(t) = 0.5*sin(20*t)")
+hold off
+legend(Location="southeast")
+
+%% Part 2
+clear; clc; close all;
+
+% Used for ode45
+tspan = [0, 50];
+
+% True mass of the system
+theta_star = @(t) 1 + 0.5*sin(0.5*t);
+
+% omega signal
+omega = @(t) sin(t);
+
+% disturbance signal
+d = @(t) 0;
+
+% Initial condition
+theta0 = 0;
+
+% Estimator gain
+
+figure
+hold on
+for g = [0.1, 1, 5]
+
+% Creating function to be estimated
+estimator = @(t, theta) gradient_estimator(t, theta, omega(t), d(t), theta_star(t), g);
+
+sol = ode45(estimator, tspan, theta0);
+
+
+plot(sol.x, sol.y, 'DisplayName',['g = ', num2str(g)])
+
+end
+
+title("w(t) = sin(t) d(t) = 0")
+hold off
+legend(Location="southeast")
+
+% disturbance signal
+d = @(t) 0.5*sin(20*t);
+
+% Initial condition
+theta0 = 0;
+
+% Estimator gain
+
+figure
+hold on
+for g = [0.1, 1, 5]
+
+% Creating function to be estimated
+estimator = @(t, theta) gradient_estimator(t, theta, omega(t), d(t), theta_star(t), g);
 
 sol = ode45(estimator, tspan, theta0);
 
