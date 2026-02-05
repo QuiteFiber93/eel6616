8ed0d215489c08f1d96188f8ed41609c5ad350f9
QuiteFiber <djd1494@gmail.com>
2026-02-04 21:14:08 -0500

    created gradient estimator figures

--- takehome1/gradient_estimator/estimation_sim_script.m
+++ takehome1/gradient_estimator/estimation_sim_script.m
@@ -2,10 +2,15 @@ clear; clc;
 gradientEstimator = "gradient_estimator";
 gains = [0.1, 0.5, 1, 5];
 
+Simulink.sdi.clear;
+
 for n = 1:length(gains)
     close_system(gradientEstimator, 0);
     load_system(gradientEstimator);
     set_param('gradient_estimator/g', 'Gain', num2str(-gains(n)));
     set_param('gradient_estimator/theta', 'SignalName', ['g = ', num2str(gains(n))]);
+    set_param('gradient_estimator/true', 'SignalName', 'True');
     simOut = sim(gradientEstimator);
 end
+
+Simulink.sdi.view;
--- takehome1/recurstive_ls/estimation_sim_script.m
+++ takehome1/recurstive_ls/estimation_sim_script.m
@@ -1,5 +1,21 @@
 clear; clc;
-ls_estimator = 'ls_estimator';
-P0 = 2;
+lsEstimator = 'ls_estimator';
 
-lambda = 0.5;
+P0s = [2, 10];
+lambdas = [0, 0.5, 1.5];
+
+Simulink.sdi.clear;
+
+for m = 1:length(P0s)
+P0 = P0s(m);
+    for n = 1:length(lambdas)
+        close_system(lsEstimator, 0);
+        load_system(lsEstimator);
+        set_param('ls_estimator/lambda', 'Value', num2str(lambdas(n)));
+        set_param('ls_estimator/theta', 'SignalName', ['P0 = ', num2str(P0), ', lambda = ', num2str(lambdas(n))]);
+        set_param('ls_estimator/true', 'SignalName', 'True');
+        simOut = sim(lsEstimator);
+    end
+end
+
+Simulink.sdi.view;
