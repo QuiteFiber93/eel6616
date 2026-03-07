fb2a3dc94cd672e91b0dfc5c13220f4e35eb3f6b
QuiteFiber <djd1494@gmail.com>
2026-03-07 18:37:00 -0500

    Final checks on 3

--- takehome2/problem3_setup.m
+++ takehome2/problem3_setup.m
@@ -4,7 +4,7 @@ modelname = 'problem3';
 
 alpha_1 = 0.0952;
 beta_1 = -0.9048;
-theta_star = [alpha_1; beta1];
+theta_star = [alpha_1, beta_1];
 theta0 = zeros(2, 1);
 P0 = 1000 * eye(2);
 
@@ -13,22 +13,72 @@ lambda = 1;
 
 out = sim(modelname);
 
-theta_ts = out.logsout.get('theta').Values;
-y_ts = out.logsout.get('y').Values;
+theta_estimate = out.logsout.get('theta').Values;
+y = out.logsout.get('y').Values;
 
 % Plotting the results
-figure;
+figure
+t = tiledlayout(2, 1,'TileSpacing','tight','Padding','none');
+nexttile;
+
 hold on
-plot(theta_ts.Time, squeeze(theta_ts.Data));
-plot(y_ts.Time, squeeze(y_ts.Data))
-grid on;
-legend('\alpha_1 (Estimate)', '\beta_1 (Estimate)', 'y(k)');
-title('Recursive Least Squares Parameter Convergence');
-xlabel('Time Steps');
-legend('Location','best')
+stairs(theta_estimate.Time, theta_estimate.Data(:, 1), 'DisplayName', '\alpha_1 Estimate');
+stairs(theta_estimate.Time, theta_estimate.Data(:, 2), 'DisplayName', '\beta_1 Estimate');
 hold off
 
-for lambda = [0.9, 1]
-    for coeff = [100, 1000, 10000]
-    end
+xlabel('Time Steps')
+ylabel('Estimate')
+ylim([-1, 0.2])
+title('Parameter Estimates');
+
+legend('Location','best')
+grid
+
+nexttile;
+stairs(y.Time, y.Data, 'DisplayName', 'y')
+
+xlabel('Time Steps')
+ylabel('Output')
+ylim([0, 1.1])
+title('y(k)')
+grid
+
+figure
+t = tiledlayout(2, 1,'TileSpacing','tight','Padding','none');
+nexttile;
+
+hold on
+for coeff = [100, 1000, 10000]
+    P0 = coeff * eye(2);
+    out = sim(modelname);
+    theta_estimate = out.logsout.get('theta').Values;
+    err = theta_estimate.Data - theta_star;
+    stairs(theta_estimate.Time, log(vecnorm(err, 2, 2).^2), 'DisplayName', ['P_0 = ', num2str(coeff), '\itI\rm'])
 end
+hold off
+
+set(gca, 'xtick', 0:25:100)
+grid
+legend
+title('\lambda = 1')
+
+xlabel('Time Step')
+ylabel('log(Error Norm Squared)')
+
+nexttile;
+lambda = 0.9;
+
+hold on
+for coeff = [100, 1000, 10000]
+    P0 = coeff * eye(2);
+    out = sim(modelname);
+    theta_estimate = out.logsout.get('theta').Values;
+    err = theta_estimate.Data - theta_star;
+    stairs(theta_estimate.Time, log(vecnorm(err, 2, 2).^2), 'DisplayName', ['P_0 = ', num2str(coeff), '\itI\rm'])
+end
+hold off
+
+set(gca, 'xtick', 0:25:100)
+grid
+legend
+title('\lambda = 0.9')
